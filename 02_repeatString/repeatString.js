a2a549388b877c8f1a5a1e60a3714e6395d53ae4
mohsinahmxd <bmohsin785@gmail.com>
2022-04-02 00:02:14 +0100

    exercise 2 complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
 
+const repeatString = function(string, num) {
+    let c = ""; // variable that stores a string which i add to each loop
+    for (let i = 0; i < num; i++) { 
+        c += string; // adding to the variable holding the string
+    }
+
+    string = ""; // empty the string now, we need to store the new repeated version in here 
+    string += c; // the variable c has now been appended to the required amount of times so now I add it onto string 
+    return string // return string 
 };
 
 // Do not edit below this line
