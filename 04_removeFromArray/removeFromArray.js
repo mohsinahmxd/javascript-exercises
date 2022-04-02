2bba22a69634f7fc989b0b4b055961fb4e29a924
mohsinahmxd <bmohsin785@gmail.com>
2022-04-02 17:08:19 +0100

    exercise 4 complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+   
+    let newArray = array.filter(function(item, index) {
+        if (!args.includes(item)) {
+            return item;
+        }
+    })
+    return newArray
+    }
 
-};
+    // filters out a new array based on arguments 
 
 // Do not edit below this line
 module.exports = removeFromArray;
