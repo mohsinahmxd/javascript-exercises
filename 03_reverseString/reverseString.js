f55757e258990df5f28ffeda9a9a17ffecaea668
mohsinahmxd <bmohsin785@gmail.com>
2022-04-02 12:57:47 +0100

    exercise 3 complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let splitstring = string.split('');  // split the string into individual characters and store in splitstring array. We need to store it as split method is non destructive
+    splitstring.reverse(); // reverse the array of characters
+    string = splitstring.join(''); // join the characters back together and assign to string
+
+    return string 
+
 
 };
 
