4b0edc569903b2eaf33faf26e3b258145203ac1e
mohsinahmxd <bmohsin785@gmail.com>
2022-04-03 17:03:17 +0100

    modified to pass remaining tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,11 +1,24 @@
 const sumAll = function(num1, num2) {
-    for (let i = num1; i != num2; i++) { 
+    if (num1 < num2 && num1 & num2 > 0 && !isNaN(num1 & num2)) { // added to pass check 3, 4, 5 and 6 
+        for (let i = num1; i != num2; i++) { 
         num1 += (i + 1);
-    }
+        }
 
-    return num1
+        return num1
+
+    } else if (num1 > num2 && num1 & num2 > 0 && !isNaN(num1 & num2)) {
+        [num1, num2] = [num2, num1]; // swapping the variable values
+        for (let i = num1; i != num2; i++) { 
+            num1 += (i + 1);
+        }
+    
+        return num1
+    } else
+        return 'ERROR'
+    
 
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
